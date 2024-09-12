162797caec5a234b308ff9520eb3cf4f2f74360b
Asif Shaik <asifpg0524@gmail.com>
2024-09-12 12:20:21 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,7 +1,7 @@
 class factorial{  
  public static void main(String args[]){  
   int i,fact=1;  
-  int number=17;//It is the number to calculate factorial    
+  int number=7;//It is the number to calculate factorial    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
